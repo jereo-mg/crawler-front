203bad7a5d72cd8a0128b39121344b653c7d90f4
Mahefa Abel <abelmahefa@gmail.com>
2022-03-18 17:18:15 +0300

    Correction project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'Crawler Front';
   result: number;
   resultStr: object;
   resultSpread: number[] = [];
