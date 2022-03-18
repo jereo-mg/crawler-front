ff8f60444f94204146088d204cf052516e4627a4
rovafitiavana <rova.randrianarisoa@pulse.mg>
2022-03-18 07:58:18 +0300

    PadEnd fucntion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'test';
   result: number;
+  resultStr: object;
+  resultSpread: number[] = [];
+  arrayNumber1: number[] = [1, 2, 3];
+  arrayNumber2: number[] = [4, 5, 6, 7, 8];
+  arrayNumber3: number[] = [200, 201];
 
   constructor(){
+    this.result = this.addition(2, 3, 8, 9, 123, 45, 4, 6);
+    // this.resultSpread = this.resultSpread.concat(this.arrayNumber1, this.arrayNumber2);
+    // this.resultSpread = ...this.arrayNumber1;
+    // this.resultSpread = [...this.arrayNumber1, ...this.arrayNumber3, ...this.arrayNumber2];
 
+    // this.resultStr = this.createObjectNimporteQuoi();
+    // console.log("this.resultStr", this.resultStr);
+
+    // const [attribute1] = this.createObjectNimporteQuoi();
+    // console.log("attribute1:", attribute1);
   }
 
-  addition(a: number, b: number): number{
-    return a + b;
+  addition(...b: number[]): number{
+    let result = 0;
+    b.forEach((bItem: number) => result += bItem);
+    return result;
+  }
+
+  createObjectNimporteQuoi(){
+    return [
+      "attribute1Value1",
+      "attribute1Value2",
+      "attribute1Value3",
+    ];
   }
 }
